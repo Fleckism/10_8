9d75240490fffb5c51e16b2c52b01b9efd487c40
Jamie Fleck <Jamie.A.Fleck@gmail.com>
2020-11-13 14:01:36 -0500

    Add project files.

---
+++ 10_8.cpp
@@ -0,0 +1,26 @@
+/* 10_8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+Create 1 array of colors red,blue,green,oragne,yellow, brown
+Array 2 distance 7, 25, 20,10,15
+Randomly select one of the above, then give a friend or foe signal then a random delay then continue
+
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
